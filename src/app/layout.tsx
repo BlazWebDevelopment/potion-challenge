ad49a629eb9c9d98193df39962c81fe61b93889a
BlazDezman <moxy.blaz@gmail.com>
2025-02-13 23:13:01 +0100

    feat/added-analitycs & custom scrollbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,6 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased px-20 py-10 max-lg:px-6 max-lg:py-4`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
